977a7eb29e2994513146e7f24e4ab611bfa9c250
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-11-04 12:34:31 -0700

    Abstracted bottom bar into its own file and setup proper start and stop actions.

---
+++ cli/src/flows/bottom-bar.js
@@ -0,0 +1,39 @@
+import chalk from 'chalk';
+import { ui } from 'inquirer';
+
+const bar = new ui.BottomBar();
+bar.rl.output.mute();
+
+let text;
+let menuOn = false;
+setInterval(() => {
+    let updated = true;
+    if (menuOn) {
+        updated = false;
+        bar.rl.output.unmute();
+        if (!text) {
+            bar.updateBottomBar(chalk.gray('─────────────────'));
+        } else {
+            bar.updateBottomBar(chalk.gray(`─────${text}────────────`));
+        }
+    }
+    if (!menuOn && !updated) {
+        updated = true;
+        bar.rl.output.mute();
+    }
+}, 1);
+
+bar.setText = (t) => { text = t; };
+bar.removeText = () => { text = undefined; };
+
+bar.toggleOff = (p) => {
+    menuOn = false;
+    return p;
+};
+
+bar.toggleOn = (p) => {
+    menuOn = true;
+    return p;
+};
+
+export default bar;
--- cli/src/flows/menu.js
+++ cli/src/flows/menu.js
@@ -1,39 +1,10 @@
-import chalk from 'chalk';
+import _ from 'lodash';
 import transmute from 'transmutation';
-import { prompt, Separator, ui } from 'inquirer';
+import { prompt, Separator } from 'inquirer';
+import bar from './bottom-bar';
 import * as prompts from '../prompts'; // eslint-disable-line import/no-unresolved, import/extensions
 
 let timer;
-const bar = new ui.BottomBar();
-bar.rl.output.mute();
-
-let menuOn = false;
-setInterval(() => {
-    let updated = true;
-    if (menuOn) {
-        updated = false;
-        bar.rl.output.unmute();
-        if (!timer) {
-            bar.updateBottomBar(chalk.gray('─────No Timer────────────'));
-        } else {
-            bar.updateBottomBar(chalk.gray(`─────${Date.now()}────────────`));
-        }
-    }
-    if (!menuOn && !updated) {
-        updated = true;
-        bar.rl.output.mute();
-    }
-}, 1);
-
-bar.toggleOff = (p) => {
-    menuOn = false;
-    return p;
-};
-
-bar.toggleOn = (p) => {
-    menuOn = true;
-    return p;
-};
 
 const bottom = new Separator();
 const menu = prompts.menu;
@@ -41,8 +12,22 @@ menu[0].choices.push(bottom);
 
 export default () => transmute()
     .do(() => bar.toggleOn())
-    .extend(() => prompt(menu))
+    .extend(() => {
+        const modifiedMenu = [{
+            ...menu[0],
+            choices: timer
+                ? _.filter(menu[0].choices, o => o.value !== 'start')
+                : _.filter(menu[0].choices, o => o.value !== 'stop'),
+        }];
+        return prompt(modifiedMenu);
+    })
     .switch('action', {
         start: transmute()
-            .do(() => { timer = true; }),
+            .do(() => { timer = setInterval(() => bar.setText(Date.now()), 0); }),
+        stop: transmute()
+            .do(() => {
+                bar.removeText();
+                clearInterval(timer);
+                timer = undefined;
+            }),
     });
